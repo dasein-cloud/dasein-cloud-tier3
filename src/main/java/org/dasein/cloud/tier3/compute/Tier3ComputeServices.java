b2983a15e1db2d3c769d8d7cbc4dff875c2bcc8b
David R Young <david.young@savvis.com>
2014-06-05 09:04:18 -0500

    Added VM & Image support.

--- src/main/java/org/dasein/cloud/tier3/compute/Tier3ComputeServices.java
+++ src/main/java/org/dasein/cloud/tier3/compute/Tier3ComputeServices.java
@@ -1,35 +1,72 @@
-/**
- * Copyright (C) 2013 Dell, Inc.
- * See annotations for authorship information
- *
- * ====================================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ====================================================================
- */
-
 package org.dasein.cloud.tier3.compute;
 
+import javax.annotation.Nonnull;
+
 import org.dasein.cloud.compute.AbstractComputeServices;
+import org.dasein.cloud.compute.MachineImageSupport;
+import org.dasein.cloud.compute.VirtualMachineSupport;
 import org.dasein.cloud.tier3.Tier3;
+import org.dasein.cloud.tier3.compute.image.Tier3Image;
+import org.dasein.cloud.tier3.compute.vm.Tier3VM;
 
-/**
- * [Class Documentation]
- * <p>Created by George Reese: 5/28/13 6:24 PM</p>
- *
- * @author George Reese
- */
 public class Tier3ComputeServices extends AbstractComputeServices {
-    private Tier3 provider;
+	private Tier3 provider;
 
-    public Tier3ComputeServices(Tier3 provider) { this.provider = provider; }
+	public Tier3ComputeServices(@Nonnull Tier3 provider) {
+		this.provider = provider;
+	}
+
+	@Override
+	public boolean hasVirtualMachineSupport() {
+		return true;
+	}
+
+	@Override
+	public VirtualMachineSupport getVirtualMachineSupport() {
+		return new Tier3VM(provider);
+	}
+
+	@Override
+	public boolean hasImageSupport() {
+		return true;
+	}
+
+	@Override
+	public MachineImageSupport getImageSupport() {
+		return new Tier3Image(provider);
+	}
+
+	@Override
+	public boolean hasVolumeSupport() {
+		return false;
+	}
+
+//	@Override
+//	public VolumeSupport getVolumeSupport() {
+//		return new Tier3Storage(provider);
+//	}
+
+	@Override
+	public boolean hasAutoScalingSupport() {
+		return false;
+	}
+
+//	@Override
+//	public AutoScalingSupport getAutoScalingSupport() {
+//		return new Tier3AutoScaling(provider);
+//	}
+
+	@Override
+	public boolean hasSnapshotSupport() {
+		return false;
+	}
+
+//	@Override
+//	public SnapshotSupport getSnapshotSupport() {
+//		return new Tier3Snapshot(provider);
+//	}
+	
+	protected Tier3ComputeTranslations getComputeTranslations() {
+		return new Tier3ComputeTranslations();
+	}
 }
