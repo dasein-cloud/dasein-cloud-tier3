b66ff5d0c7500990b8ac74be9cf07bbc94faa1f7
David R Young <david.young@savvis.com>
2014-06-08 20:50:57 -0500

    Initial add of new OS object

---
+++ src/main/java/org/dasein/cloud/tier3/compute/Tier3OS.java
@@ -0,0 +1,16 @@
+package org.dasein.cloud.tier3.compute;
+
+public class Tier3OS {
+	public int id;
+	public String name;
+	public int maxCpu;
+	public int maxMemory;
+
+	public Tier3OS(int id, String name, int maxCpu, int maxMemory) {
+		this.id = id;
+		this.name = name;
+		this.maxCpu = maxCpu;
+		this.maxMemory = maxMemory;
+	}
+}
+
